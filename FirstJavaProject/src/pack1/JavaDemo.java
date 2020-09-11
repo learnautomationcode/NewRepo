2f246a04141d2de74e36b268d99fd04f81d47048
learnautomationcode <learnautomation88@gmail.com>
2020-09-11 13:23:07 +0530

    first commit

---
+++ FirstJavaProject/src/pack1/JavaDemo.java
@@ -0,0 +1,22 @@
+package pack1;
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("This is my first session of JAVA");
+		
+		System.out.println("This is my second session of JAVA");
+		
+		System.out.println("I hope i will learn soon");
+		
+		String carname="Ferrari";
+		
+		System.out.println(carname+" is my car");
+		
+
+	}
+
+}
